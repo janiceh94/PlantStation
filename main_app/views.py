4707b3459349703cefe089484e8e217e39f7d1df
janiceh94 <haesung94@gmail.com>
2022-04-16 13:07:21 -0700

    search bar working

--- main_app/views.py
+++ main_app/views.py
@@ -9,6 +9,11 @@ class Plant_List(TemplateView):
    template_name = 'plant_list.html'
  
    def get_context_data(self, **kwargs):
-       context = super().get_context_data(**kwargs)
-       context['plants'] = Plant.objects.all()
-       return context
+    context = super().get_context_data(**kwargs)
+    name = self.request.GET.get('name')
+    if name != None:
+        context['plants'] = Plant.objects.filter(name__icontains=name)
+    else:
+        context['plants'] = Plant.objects.all()
+    return context
+
