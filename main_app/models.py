fb92b9bdd42a2d0e1a22ecc2561c20662b33342b
janiceh94 <haesung94@gmail.com>
2022-04-16 12:50:33 -0700

    created plant model and auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=250)
+    img = models.ImageField(upload_to='images')
+    water = models.CharField(max_length=250)
+    light = models.CharField(max_length=250)
+    temperature = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from .models import Plant
  
 class Landing(TemplateView):
    template_name = 'landing.html'
- 
-class Plant:
-   def __init__(self, name):
-       self.name = name
- 
-plants = [
-   Plant('Monstera'),
-   Plant('Alocasia'),
-]
+
 class Plant_List(TemplateView):
    template_name = 'plant_list.html'
  
    def get_context_data(self, **kwargs):
        context = super().get_context_data(**kwargs)
-       context['plants'] = plants
+       context['plants'] = Plant.objects.all()
        return context
