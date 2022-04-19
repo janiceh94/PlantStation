b9b9ead8d8df3693ce68dc06c759fb33f44eb32b
janiceh94 <haesung94@gmail.com>
2022-04-19 16:27:43 -0700

    added soil model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Plant
+from .models import Plant, Soil
 
 admin.site.register(Plant)
+admin.site.register(Soil)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Soil(models.Model):
+    name = models.CharField(max_length=250)
+    img = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.name
+
 class Plant(models.Model):
     name = models.CharField(max_length=250)
     img = models.ImageField(upload_to='images')
